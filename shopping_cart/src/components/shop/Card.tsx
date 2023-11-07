2a2479c20eea6184da7a1892083732046685f835
Anh Pati <romain-felicianne@hotmail.fr>
2023-11-07 15:53:16 +0100

    Card
    Affichage des accessoires reçus.

--- shopping_cart/src/components/shop/Card.tsx
+++ shopping_cart/src/components/shop/Card.tsx
@@ -1,5 +1,16 @@
 export const Card = ({ title, description, image, price }) => {
+    const priceDecimal = (price.toString().slice(price.toString().indexOf('.') + 1).length < 2) && (price.toString().includes('.')) ? true : false
+    console.log(priceDecimal)
+    console.log('Taille')
+    console.log(title)
+    console.log(price.toString().slice(price.toString().indexOf('.')).length)
+
     return (
-        <div>Card</div>
+        <li>
+            <h4>{title}</h4>
+            <img src={image} alt={title} />
+            <p>{description}</p>
+            <h3>{price + (priceDecimal && '0')} €</h3>
+        </li>
     )
 }
