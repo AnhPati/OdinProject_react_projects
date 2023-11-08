9abe72e6c2627b3381d4be9c7c53b64eff35211e
Anh Pati <romain-felicianne@hotmail.fr>
2023-11-08 19:18:09 +0100

    Navbar styles
    Stylisation de la Navbar.
    Recherche en cours pour changer les couleurs selon la page sur laquelle on est. Tests avec le style conditionnel de styled-components mais problème avec la props.

--- shopping_cart/src/components/Navbar.tsx
+++ shopping_cart/src/components/Navbar.tsx
@@ -1,32 +1,85 @@
 import { NavLink, useLocation } from "react-router-dom"
+import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleLeft } from "@fortawesome/free-solid-svg-icons"
 import { faStore } from "@fortawesome/free-solid-svg-icons/faStore"
 import { CartButton } from "./utils/CartButton"
 
+const NavbarContainer = styled.nav`
+  background: ${currentPage => currentPage ? '#FFBA18' : '#74DAF8'};
+  color: ${currentPage => currentPage ? '#582D1D' : '#1D3E56'};
+  font-family: 'Josefin Sans', sans-serif;
+  padding: 1.4em 0.3em
+`
+const NavbarList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`
+
+const NavbarLink = styled.li`
+  svg {
+    font-size: 2em;
+    color: ${currentPage => currentPage ? '#FFF7C2' : '#E1F6FD'};
+    padding: 0 0.6em;
+  }
+
+  &:hover,
+  &:focus {
+    svg {
+      color: ${currentPage => currentPage ? '#FFEE9C' : '#D1F0FA'};
+    }
+  }
+
+  &:active {
+    svg {
+      color: ${currentPage => currentPage ? '#FBE577' : '#BEE7F5'};
+    }
+  }
+`
+
+const NavbarTitleContainer = styled.li`
+  flex-grow: 2;
+  text-align: center;
+`
+
+const NavbarTitle = styled.h2`
+  margin: 0;
+  text-transform: uppercase;
+  font-size: 2.8em;
+  font-weight: 700;
+`
+
 const Navbar = ({ productsCart }) => {
   const location = useLocation()
 
-  const nextPage = location.pathname === '/shop' ? '' : 'shop'
-  const nextPageIcon = location.pathname === '/shop' ? faCircleLeft : faStore
-  const pageTitle = location.pathname === '/shop' ? 'Shop' : 'Home'
+  const isShop = location.pathname === '/shop' ? true : false
+  const nextPage = isShop ? '' : 'shop'
+  const nextPageIcon = isShop ? faCircleLeft : faStore
+  const pageTitle = isShop ? 'Shop' : 'Home'
+
+  console.log(isShop)
+
 
   return (
-    <div>
-      <ul>
-        <li>
+    <NavbarContainer currentPage={isShop} >
+      <NavbarList>
+        <NavbarLink>
           <NavLink to={`/${nextPage}`} >
             <FontAwesomeIcon icon={nextPageIcon} />
           </NavLink>
-        </li>
-        <li>
-          <h2>{pageTitle}</h2>
-        </li>
-        <li>
+        </NavbarLink>
+        <NavbarTitleContainer>
+          <NavbarTitle>{pageTitle}</NavbarTitle>
+        </NavbarTitleContainer>
+        <NavbarLink>
           <CartButton productsNumber={productsCart.length} />
-        </li>
-      </ul>
-    </div>
+        </NavbarLink>
+      </NavbarList>
+    </NavbarContainer>
   )
 }
 
