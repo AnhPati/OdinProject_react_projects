851cbf47685ed305a323b604ff86f0a2e966d1ce
Anhpati <romain-felicianne@hotmail.fr>
2024-09-25 18:21:53 +0200

    🐛 CART > Add id to Cart and add disabled CartButton to Home page.

--- shopping_cart/src/components/utils/CartButton.tsx
+++ shopping_cart/src/components/utils/CartButton.tsx
@@ -1,33 +1,37 @@
-import { NavLink } from "react-router-dom"
 import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons/faCartShopping"
 
-const CartContainer = styled.div<{ $colorShop?: boolean }>`
-    position: relative;
-`
 
-const CartNumber = styled.span<{ $colorShop?: boolean }>`
-    position: absolute;
-    top: 3.5px;
-    right: 29px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: ${props => props.$colorShop ? '#AB6400' : '#00749E'};
-    }
-`
-
-
-export const CartButton = ({ productsNumber = 0, colorShop }) => {
+export const CartButton = ({ productsNumber = 0, colorShop, disabled }) => {
     return (
-        <NavLink to={`#`}>
-            <CartContainer $colorShop={colorShop}>
+        <CartContainerStyled $colorShop={colorShop} $disabled={disabled}>
+            <a href={`#cart`} className="cart-button">
                 <FontAwesomeIcon icon={faCartShopping} />
-                <CartNumber $colorShop={colorShop}>
+                <span className="cart-number">
                     {productsNumber}
-                </CartNumber>
-            </CartContainer>
-        </NavLink>
+                </span>
+            </a>
+        </CartContainerStyled>
     )
 }
+
+const CartContainerStyled = styled.div<{ $colorShop?: boolean, $disabled?: boolean }>`
+    cursor: ${props => props.$disabled ? 'not-allowed' : 'pointer'};
+
+    .cart-button {
+        display: block;
+        position: relative;
+        pointer-events: ${props => props.$disabled ? 'none' : 'auto'};
+        
+        .cart-number {
+            position: absolute;
+            top: 3.5px;
+            right: 29px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: ${props => props.$colorShop ? '#AB6400' : '#00749E'};
+        }
+    }
+`
