71ebac28ab4a0080e37e5032f1fbc0765180fe1b
Anhpati <romain-felicianne@hotmail.fr>
2024-09-25 20:53:58 +0200

    🐛 CART > Display product to pay.

--- shopping_cart/src/components/shop/Cart.tsx
+++ shopping_cart/src/components/shop/Cart.tsx
@@ -4,19 +4,20 @@ const Cart = ({ productsCart }) => {
     const amount = productsCart.reduce((acc, currentValue) => acc + Number(currentValue.quantity) * currentValue.price, 0)
     const amountString = amount.toString()
     const amountDecimal = (amountString.slice(amountString.indexOf('.') + 1).length < 2) && (amountString.includes('.')) ? true : false
+    console.log(productsCart)
     return (
         <CartStyled id="cart">
             <form action="submit">
                 <h2>Récapitulatif de votre commande</h2>
                 <ul>
-                    {productsCart.map(product => {
+                    {productsCart.map(product =>
                         <li key={product.id}>
                             <p>{product.title}</p>
                             <p>Prix unitaire : {product.price}</p>
                             <p>Quantité : {product.quantity}</p>
                             <img src={product.image} alt={product.title} />
                         </li>
-                    })}
+                    )}
                 </ul>
                 <hr />
                 <h2>Réglez votre commande</h2>
@@ -36,6 +37,15 @@ const CartStyled = styled.div`
     flex-direction: column;
     align-items: center;
     font-family: 'Josefin Sans', sans-serif;
+
+    ul {
+        padding-left: 0;
+        list-style: none;
+    }
+
+    img {
+        max-width: 70px;
+    }
 
     .cart-pay {
         display: flex;
