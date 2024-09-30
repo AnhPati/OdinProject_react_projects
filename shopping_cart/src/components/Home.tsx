85a85c3fa455e2c4ae3230c181f4a5a46c790c4e
Anhpati <romain-felicianne@hotmail.fr>
2024-09-30 23:56:18 +0200

    🚧 HOME > Add home title and background animation.

--- shopping_cart/src/components/Home.tsx
+++ shopping_cart/src/components/Home.tsx
@@ -1,9 +1,26 @@
+import styled from "styled-components";
+
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <HomeStyled>
+            <h1>Urban Avenue</h1>
+        </HomeStyled>
     )
 }
 
 export default Home
+
+const HomeStyled = styled.main`
+    height: 100vh;
+    background: #1D3E56;
+    animation: fadeBackgroundColor 5000ms ease-in;
+
+    @keyframes fadeBackgroundColor {
+        0% {
+            background: #F1FAFD;
+        }
+        100% { 
+            background: #1D3E56;
+        }
+    }
+`;
